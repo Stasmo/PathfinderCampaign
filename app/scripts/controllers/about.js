46638c01acc93f752e708243cac0c1f1b446e233
Simon Staszkiewicz <simonstaszkiewicz@gmail.com>
2015-02-22 17:16:14 -0800

    added app directory

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pathfinderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pathfinderApp
+ */
+angular.module('pathfinderApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
